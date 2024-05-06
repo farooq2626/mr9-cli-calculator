87ebee51c0a4a17061dc23c8d4ba7facdde3cf1e
Your Name <you@example.com>
2024-05-07 03:58:37 +0500

    check plz

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 #!/user/bin/env node
 import inquirer from "inquirer";
 const answer= await inquirer.prompt([
-    {message:"enterfirstnumber",type: "number",name:"firstnumber"},
-    {message:"entersecondnumber",type:"number",name:"secondnumber"},
+    {message:"enter myfirstnumber",type: "number",name:"firstnumber"},
+    {message:"enter mysecondnumber",type:"number",name:"secondnumber"},
     {message:"select one of the operator to perform operator",
     type:"list",
     name:"operator",
@@ -17,8 +17,8 @@ const answer= await inquirer.prompt([
   }else if(answer.operator === "division") {
     console.log(answer.firstnumber / answer.secondnumber);
     }else{
-        console.log("please select valid operator")
+        console.log("please select valid operator");
     }
-    
+       console.log("THE END");
  
 
